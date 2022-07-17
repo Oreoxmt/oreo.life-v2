602f572341ea74e9e772461f16c95e815e0370ba
Aolin <aolin.zhang@pingcap.com>
2022-07-17 21:49:55 +0800

    init

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'On my way to Oodi',
   tagline: '"Siempre imaginé que el paraíso sería algún tipo de biblioteca"',
-  url: 'https://v2.oreo.life',
+  url: 'https://oreolife.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
