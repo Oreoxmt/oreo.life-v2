6ff31119070431e34838dfd83f8548becf0a1274
Aolin <aolinz@outlook.com>
2023-02-26 18:10:23 +0800

    add additional language: rust and cpp
    Signed-off-by: Aolin <aolinz@outlook.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -144,7 +144,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['docker'],
+        additionalLanguages: ['docker', 'rust', 'cpp'],
       },
       colorMode: {
         respectPrefersColorScheme: true,
